4f36876b11e8b7068b2a0d3e67c83b283f8751b9
TraiW <valentinw@hotmail.fr>
2020-03-24 16:16:56 +0100

    Push webhook functionnel en local

--- app.js
+++ app.js
@@ -21,7 +21,7 @@
  */
 
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = "EAALERtTvD8wBADbNvg1ZC1yn7XcDwz1vjKspcg5r0qpvZBU0U7BhN7w2K7DaZB8OZBV96R5cc6ecwroKy4cJyOXDJASBCJ6fJKJVsjXIr6i9Tcjd95pvMK10h83D3uuJkTcRUB7Yb0iskzM04HKZCZAkA1qBvMLsUirKW8PlOcPTYMtmE73LFZCGbvXXJsTFncZD";
 // Imports dependencies and set up http server
 const 
   request = require('request'),
@@ -32,41 +32,26 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Accepts POST requests at /webhook endpoint
+// Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
-
-  // Parse the request body from the POST
+ 
   let body = req.body;
-
-  // Check the webhook event is from a Page subscription
+  // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
+    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the body of the webhook event
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: ' + sender_psid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
-        
-        handlePostback(sender_psid, webhook_event.postback);
-      }
-      
     });
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
 
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
   } else {
-    // Return a '404 Not Found' if event is not from a page subscription
+    // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
@@ -76,7 +61,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR VERIFY TOKEN>";
+  const VERIFY_TOKEN = "0478455304";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
