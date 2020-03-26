7bfc793eab1c233a46e3a840a63acfbd56e6b7e0
TraiW <valentinw@hotmail.fr>
2020-03-26 11:44:41 +0100

    Correction

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const 
   request = require('request'),
